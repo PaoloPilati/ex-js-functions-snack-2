10354cfb59878322098b19e1e08da2c3be74352b
Zizou10 <paolo.pilati93@gmail.com>
2026-06-20 00:52:47 +0200

    Soluzione Snack 3.

---
+++ Snack3.js
@@ -0,0 +1,11 @@
+//Snack 3
+//Crea una funzione eseguiOperazione
+//Definisci una funzione eseguiOperazione che accetta tre parametri: due numeri e una funzione operatore (callback). La funzione deve eseguire l'operazione fornita sui due numeri.
+
+const performOperation = (x, y, operation) => operation(x, y);
+
+const sum = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+
+console.log(performOperation(5, 5, sum));
+console.log(performOperation(5, 5, multiply));
