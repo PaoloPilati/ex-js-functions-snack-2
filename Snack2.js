82cb435927d773ff09991710afe9b483b31df803
Zizou10 <paolo.pilati93@gmail.com>
2026-06-20 00:43:54 +0200

    Soluzione x Snack 2

---
+++ Snack2.js
@@ -0,0 +1,13 @@
+//Snack 2
+//Crea una arrow function che calcola il quadrato di un numero.
+//Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+//function square(squaredNumber) {
+//    const result = squaredNumber * squaredNumber;
+//    return result;
+//}
+
+const square = (squaredNumber) => 
+    squaredNumber * squaredNumber;
+
+console.log(square(5));
