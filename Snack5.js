8a069d8b340a6236973fbe3b11aeb3d6fe81769e
Zizou10 <paolo.pilati93@gmail.com>
2026-06-22 22:02:41 +0200

    Soluzione parziale Snack5 (creazione loop infinito con messaggio).

---
+++ Snack5.js
@@ -0,0 +1,13 @@
+//Snack 5
+//Crea una funzione stampaOgniSecondo con setInterval.
+//Definisci una funzione che accetta un messaggio e lo stampa ogni secondo.
+
+//Nota: Questa funzione creerà un loop infinito. Interrompilo manualmente o usa clearInterval() in un altro script.
+
+printEverySecond = (message) => console.log(message);
+
+setInterval(() => {
+    printEverySecond("Print me after a while!");}, 1000);
+
+printEverySecond("Print me now.");
+
